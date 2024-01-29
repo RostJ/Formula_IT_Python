a55277e214c2b57e141b62fa0b552d489e89b129
Платухин Ростислав <147276471+RostJ@users.noreply.github.com>
2024-01-29 22:38:33 +0300

    Add files via upload

---
+++ Лабораторная работа №3.py
@@ -0,0 +1,96 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        self._author = author
+
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name=name, author=author)
+        self._pages = pages
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages):
+
+        if not isinstance(pages, int):
+            raise TypeError
+        if pages <= 0:
+            raise ValueError
+        self._pages = pages
+
+
+    def __str__(self):
+        return f"Бумажная книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self._pages!r})"
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name=name, author=author)
+        self._duration = duration
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration):
+        if not isinstance(duration, float):
+            raise TypeError
+        if duration <= 0:
+            raise ValueError
+        self._duration = duration
+
+
+
+    def __str__(self):
+        return f"Аудиокнига {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self._duration!r})"
+
+book = Book('Война и мир', 'Л.Н. Толстой')
+pbook = PaperBook('Война и мир', 'Л.Н. Толстой', 10)
+abook = AudioBook('Война и мир', 'Л.Н. Толстой', 3245.6)
+
+print(book, book.__repr__(), sep='\n')
+print()
+print(pbook, pbook.__repr__(), sep='\n' )
+print()
+print(abook, abook.__repr__(), sep='\n')
+print()
+
+pbook.pages = 12
+abook.duration = 1234.1
+
+
+print('Заменили параметры pages и duration', '\n')
+
+print(pbook.__repr__())
+print(abook.__repr__())
